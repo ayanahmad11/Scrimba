0cffcd9615ecea80faaf01c8ba954c1928747b40
ayanahmed05 <ayan005.aa@gmail.com>
2024-07-31 18:39:19 +0530

    lecture 1

---
+++ lec1/index.js
@@ -0,0 +1,6 @@
+ReactDOM.render( <p> Hello!,My name is bob </p>,document.getElementById("root")); 
+ReactDOM.render( <h1> Hello!My name is Ayan </h1>,document.querySelector('#root')); 
+ReactDOM.render(<ul><li>Thing 1 </li><li>Thing 2
+</li></ul>,document.getElementById("root"))
+// ReactDOM.render( <h1> Hello!,Everyone</h1>,document.getElementById("root"))    
+//* notice that the latest code that access root element will be implemented like Thing 1 and thing 2 as Ul in root
