2b278b7b5ca422d366199e33ceb9d701fb6fa44d
ayanahmed05 <ayan005.aa@gmail.com>
2024-07-31 22:36:49 +0530

    cl

--- lec1/index.js
+++ lec1/index.js
@@ -3,4 +3,4 @@ ReactDOM.render( <h1> Hello!My name is Ayan </h1>,document.querySelector('#root'
 ReactDOM.render(<ul><li>Thing 1 </li><li>Thing 2
 </li></ul>,document.getElementById("root"))
 // ReactDOM.render( <h1> Hello!,Everyone</h1>,document.getElementById("root"))    
-//* notice that the latest code that access root element will be implemented like Thing 1 and thing 2 as Ul in root
+//* notice that the latest code that access root element will be implemented like Thing 1 and thing 2 as Ul root     
