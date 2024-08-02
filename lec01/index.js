c59ef190f092ad7035a9acdf9cc45907d51d27b6
ayanahmad11 <ayan2011032@akgec.ac.in>
2024-08-02 21:29:35 +0530

    react .js

---
+++ lec01/index.js
@@ -0,0 +1,6 @@
+// Spend a minute memorizing the line below
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+// Or we can use document.querySelector("#id_name")
+ReactDOM.render(<h1>Hi there</h1>,document.querySelector('#root')) 
+
+//* notice that the latest code that access root element will be implemented like Thing 1 and thing 2 as Ul root     
---
+++ lec05/index.js
@@ -0,0 +1,53 @@
+const h1 = document.createElement("h1")
+h1.textContent = "Hello world"
+h1.className = "header"
+console.log(h1)
+
+// <h1 class="header">
+
+const element = <h1 className="header">This is JSX</h1>
+console.log(element)
+
+/*
+{
+    type: "h1", 
+    key: null, 
+    ref: null, 
+    props: {
+        className: "header", 
+        children: "This is JSX"
+    }, 
+    _owner: null, 
+    _store: {}
+}
+ */
+
+// JSX
+ReactDOM.render(element, document.getElementById("root"))
+
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+
+console.log(page)
+
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+)
+
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
